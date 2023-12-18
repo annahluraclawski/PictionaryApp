47131ffb091077595196ca5b5049cd416f29038c
Annahlu Raclawski <annahlu47@gmail.com>
2023-12-18 09:45:29 -0600

    Final

--- Pictionary App/ViewControllerGame.swift
+++ Pictionary App/ViewControllerGame.swift
@@ -5,11 +5,23 @@
 //  Created by ANNAHLU RACLAWSKI on 12/7/23.
 //
 
+public class AppData{
+    static var points = 0
+}
+
 import UIKit
 
 class ViewControllerGame: UIViewController {
 
-    var images : [UIImage] = []
+    let images = ["bombay", "calico", "bengal", "siamese", "persian", "americanshorthair", "sphynx", "russianblue", "mainecoon", "tabby", "ragdoll"]
+    
+    var final = [String]()
+    
+    var answer = ""
+    var two = ""
+    var three = ""
+    var four = ""
+    
     
     @IBOutlet weak var imageOutlet: UIImageView!
     
@@ -21,27 +33,67 @@ class ViewControllerGame: UIViewController {
     
     @IBOutlet weak var buttonFourOutlet: UIButton!
     
+    @IBOutlet weak var pointsOutlet: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        
-        images.append(UIImage(named: "bombay")!)
-        images.append(UIImage(named: "calico")!)
-        images.append(UIImage(named: "bengal")!)
-        images.append(UIImage(named: "siamese")!)
-        images.append(UIImage(named: "persian")!)
-        images.append(UIImage(named: "americanshorthair")!)
-        images.append(UIImage(named: "sphynx")!)
-        images[7] = UIImage(named: "russianblue")!
-        images[8] = UIImage(named: "mainecoon")!
-        images[9] = UIImage(named: "tabby")!
-        images[10] = UIImage(named: "ragdoll")!
-        
-        //imageOutlet.image = images[0]
+        newQuestion()
         
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if sender.currentTitle == answer {
+            print("Correct")
+            AppData.points = AppData.points + 1
+            pointsOutlet.text = "Points: \(AppData.points)"
+        }
+        else {
+            print("Wrong")
+            AppData.points = 0
+            pointsOutlet.text = "Points: \(AppData.points)"
+            let alert = UIAlertController(title: "Oops!", message: "That is incorrect, you have \(AppData.points) points", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "ok", style: .destructive, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+        }
+        newQuestion()
+    }
+    
+    
+    func newQuestion() {
+        let random = Int.random(in: 0..<images.count)
+        answer = images[random]
+        imageOutlet.image = UIImage(named: answer)
+        
+        let random2 = Int.random(in: 0..<images.count)
+        two = images[random2]
+        
+        let random3 = Int.random(in: 0..<images.count)
+        three = images[random3]
+        
+        let random4 = Int.random(in: 0..<images.count)
+        four = images[random4]
+        
+        final.append(answer)
+        final.append(two)
+        final.append(three)
+        final.append(four)
+        
+        let shuffled = final.shuffled()
+        
+            buttonOneOutlet!.setTitle(shuffled[0], for: .normal)
+            buttonTwoOutlet!.setTitle(shuffled[1], for: .normal)
+            buttonThreeOutlet!.setTitle(shuffled[2], for: .normal)
+            buttonFourOutlet!.setTitle(shuffled[3], for: .normal)
+        
+    }
+    
+    @IBAction func refreshButton(_ sender: UIBarButtonItem) {
+        newQuestion()
+    }
+    
     
 }
